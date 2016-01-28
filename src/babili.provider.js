c8552d1bb5348e422b262a8eb3e8e8bbd0734ae3
Dorian Somers <doriansomers@gmail.com>
2016-01-28 14:07:10 +0100

    change for update

--- src/babili.provider.js
+++ src/babili.provider.js
@@ -85,10 +85,12 @@
           });
           return deferred.promise;
         },
-        refresh: function (currentBabiliUser) {
-          injector.get("BabiliMe").get().$promise.then(function onSuccess(babiliUser) {
-            if (currentBabiliUser.rooms.length !== babiliUser.rooms.length) {
-              babiliUser.rooms.forEach(function(room) {
+        update: function (currentBabiliUser, token) {
+          apiToken = token;
+          var deferred = $q.defer();
+          injector.get("BabiliMe").get().$promise.then(function onSuccess(newBabiliUser) {
+            if (currentBabiliUser.rooms.length !== newBabiliUser.rooms.length) {
+              newBabiliUser.rooms.forEach(function(room) {
                 if (!currentBabiliUser.rooms.some(function hasRoom(userRoom) {
                   return userRoom.id === room.id;
                 })) {
@@ -96,7 +98,9 @@
                 }
               });
             }
+            deferred.resolve();
           });
+          return deferred.promise;
         }
       };
     };
